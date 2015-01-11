ed2d9c90dbb563b2f48f8124a61436dab59d6c46
wyqbupt <wyqcsnet@yeah.net>
2015-01-11 17:37:57 +0800

    beautify url

--- protected/config/main.php
+++ protected/config/main.php
@@ -39,15 +39,17 @@ return array(
 
 		// uncomment the following to enable URLs in path-format
 		
-		/*'urlManager'=>array(
+		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
 			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				/*'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',*/
+				'commentfeed'=>array('comment/feed','urlSuffix'=>'.xml', 'caseSensitive'=>false),
 			),
 		),
-		*/
+		
 
 		'authManager'=>array( 
 			'class'=>'CDbAuthManager', 
